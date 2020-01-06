3b3bb2c1702d875ad13c4ee854f3529cf8d029de
Mayur Patil <mayur@monkeychai.com>
2020-01-06 11:35:04 +0530

    Add Text Input field for Habits

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, TextInput, StatusBar, TouchableOpacity, Image } from 'react-native';
+
+import Frequency from './Components/utils/selection'
 
 export default class App extends React.Component {
+
+   buttonPressed() {
+      var atta = Frequency.Daily
+      console.log(atta)
+   }
+
+   handleHabit(e) {
+      console.log(e)
+   }
+
    render() {
       return (
 
-         <View style = {styles.container}>
-            <StatusBar hidden/>
-            <Text>
-               Whoops
-            </Text>
+         <View style={styles.container}>
+            <StatusBar hidden />
+
+            <TextInput
+               style={styles.input}
+               underlineColorAndroid="transparent"
+               placeholder="Add Habit.."
+               placeholderTextColor="#979681"
+               autoCapitalize="none"
+               onChangeText={this.handleHabit}
+            />
+
+            <TouchableOpacity activeOpacity={.8} style={styles.fullWidthButton} onPress={this.buttonPressed}>
+               <Text style={styles.fullWidthButtonText}>+</Text>
+            </TouchableOpacity>
+
          </View>
+
       );
    }
 }
@@ -18,8 +42,31 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#CBCCBE',
       alignItems: 'center',
+      justifyContent: 'space-between',
+   },
+   input: {
+      margin: 15,
+      height: 80,
+      backgroundColor: 'white',
+      alignSelf: 'stretch',
+      borderColor: '#E5E5D9',
+      borderRadius: 5,
+      borderWidth: 1
+   },
+   fullWidthButton: {
+      backgroundColor: 'white',
+      height: 50,
+      margin: 10,
+      alignSelf: 'stretch',
+      borderRadius: 5,
+      flexDirection: 'row',
       justifyContent: 'center',
+      alignItems: "center",
+   },
+   fullWidthButtonText: {
+      fontSize: 24,
+      color: '#979681'
    },
 });
